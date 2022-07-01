5ef29cb9558debaa6d2396744c4708edb13894e5
Rohan Shahshankar <43114348+rohanpls@users.noreply.github.com>
2022-07-01 17:11:46 +0530

    Class implementation with properties

---
+++ 3_employee_class_init.py
@@ -0,0 +1,33 @@
+# Declaring a class 'employee' with some properties
+
+class employee:
+    
+    #Logically, if properties need to be unique for the object, you can initialize them without value
+    # we will assign 'None' as default value
+
+    eID = None   #Employee ID
+    department = None  #Employee Department
+    Salary = None  #Employee Salary
+
+    # Please note, initializing property values is necessary and won't compile without it.
+
+# Creating an object of class Employee
+
+Lucas = employee()
+
+# Assigning values to our unique object
+
+Lucas.eID = 100
+Lucas.department = "IT"
+Lucas.Salary = '$90k'
+
+# Printing properties
+print('Employee ID = ', Lucas.eID)
+print('Employee Department = ', Lucas.department)
+print('Employee Salary = ', Lucas.Salary)
+
+# Python allows to create properties outside class as well:
+
+Lucas.title = 'Software Engineer'
+
+print('Employee title: ', Lucas.title)
