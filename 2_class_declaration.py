38af890f4d630f6678ca3beac209b8b43c8057f9
Rohan Shahshankar <43114348+rohanpls@users.noreply.github.com>
2022-07-01 17:11:00 +0530

    Declare a class

---
+++ 2_class_declaration.py
@@ -0,0 +1,18 @@
+# How to declare a class in Python
+
+
+class myClassName:
+
+#All Properties and methods of this class will be defined inside here
+# Easier terms for newbies: Properties -> Variables in the class
+#                           Methods -> Functions (which may or may not return values)
+
+    pass    #Python's way of saying just get it over with (if we have nothing to do here)
+
+# Creating class object:
+
+ObjectName = myClassName()
+
+print(ObjectName) # This will print the memory address where this object is stored
+
+#This is only basic implementation of a class and don't contain any properties or methods yet
