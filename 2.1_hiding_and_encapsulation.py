a3b9a0fe4915a5598d74b8ea42f35cb183da6419
Rohan Shahshankar <43114348+rohanpls@users.noreply.github.com>
2022-07-03 21:17:28 +0530

    2.1 Hiding and Encapsulation

---
+++ 2.1_hiding_and_encapsulation.py
@@ -0,0 +1,40 @@
+#In OOP, objects and classes are the fundamental entities. Objects are created using classes. 
+# One can observe that classes contain properties and that objects are created to manipulate 
+# and access these properties. To make this object-oriented system more reliable and error free,
+# it is a good practice to limit access to the class members at times.
+
+
+# Information hiding refers to the concept of hiding the inner workings of a class 
+# and simply providing an interface through which the outside world can interact 
+# with the class without knowing what’s going on inside.
+
+
+# Data hiding can be divided into two primary components:
+# 1. Encapsulation
+# 2. Abstraction
+
+
+# Chapter 2 is about Encapsulation. 
+# Abstraction will be covered in second Unit
+
+
+# 1. Encapsulation:
+# Encapsulation in OOP refers to binding data and the methods to manipulate that data together 
+# in a single unit, that is, class.
+
+# When encapsulating classes, a good convention is to declare all variables of a class private. 
+# This will restrict direct access by the code outside that class.
+
+# At this point, a question can be raised: If the methods and variables are encapsulated in a class,
+# then how can they be used outside of that class?
+
+# The answer to this is simple. One has to implement public methods to let the outside world 
+# communicate with this class. These methods are called getters and setters. We can also implement other custom methods.
+
+
+# Advantages of encapsulation 
+
+ # * Classes make the code easy to change and maintain.
+ # * Properties to be hidden can be specified easily.
+ # * We decide which outside classes or functions can access the class properties.
+
