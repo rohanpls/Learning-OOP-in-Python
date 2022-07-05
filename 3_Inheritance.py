4b93539f16d81a3a1c1e92aeff3982e4deba7451
Rohan Shahshankar <43114348+rohanpls@users.noreply.github.com>
2022-07-05 21:33:11 +0530

    3. Inhertiance in Python

---
+++ 3_Inheritance.py
@@ -0,0 +1,19 @@
+# Inheritance : Another key concept in Object Oriented Programming
+
+# It provides a way to create a new class frrom an existing class.
+# The new class is specialized version of existing class such that it inherits all non-private properties and methods from existing class ( like having a relation)
+
+# Examples of relation:
+# Rectange IS A shape
+# Python IS A programming language
+
+# With above shown relations, we can conclude that we can build new classes by extending existing classes.
+
+# Existing class:           # Derived Class
+# Shape                     # Rectangle
+# Programming Language      # Python
+
+# A little confession about python:
+
+# Whenever we create a 'class' in python, it is, by default, a subclass of built-in Python object: class. 
+# Yep.
